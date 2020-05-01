0d1ddd4afc4d37a5f0f4e714aee9a5f43eb1b5cd
stdAlexTikhonov <std1992@mail.ru>
2020-05-01 11:56:48 +0300

    TASK-5 Added types fro api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,8 @@
+import { Data_for_query } from "./types";
+
 export const post = async (
   url: string = "https://stat.world/biportal/api.jsp",
-  data: any
+  data: Data_for_query
 ) => {
   const response = await fetch(url, {
     credentials: "same-origin", // параметр определяющий передвать ли разные сессионные данные вместе с запросом
@@ -10,5 +12,5 @@ export const post = async (
   return response.json();
 };
 
-export const getData = async (data: any) =>
+export const getData = async (data: Data_for_query) =>
   await post(process.env.REACT_APP_BI_URL, data);
---
+++ src/utils/types.ts
@@ -0,0 +1,17 @@
+export interface Data_for_query {
+  session: string;
+  method: string;
+  user?: string;
+  password?: string;
+  firstName?: string;
+  surName?: string;
+  email?: string;
+  language?: string;
+  solution?: string;
+  project?: string;
+  report?: string;
+  type?: string;
+  slice?: string;
+  view?: string;
+  code?: string; //filter code
+}
