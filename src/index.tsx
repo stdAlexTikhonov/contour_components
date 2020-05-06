4f276681221dc7989e450ec93ddf5014fb1e25be
stdAlexTikhonov <std1992@mail.ru>
2020-05-06 21:33:07 +0300

    TASK-8 Added store

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import { store } from "./store/config_store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name={"Alex"} />
+    <Provider store={store}>
+      <App name={"Alex"} />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
