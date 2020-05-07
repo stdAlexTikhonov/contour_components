bbf8d4de21bdcf65e76014fe4192cc20ff510422
stdAlexTikhonov <std1992@mail.ru>
2020-05-07 21:10:13 +0300

    TASK-8 Fix

--- src/components/Navabar/index.tsx
+++ src/components/Navabar/index.tsx
@@ -3,19 +3,11 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
 import { useStyles } from "./styles";
 
 export const ButtonAppBar: React.FC = () => {
   const classes = useStyles();
-
-  const handleChange = () => console.log(1);
 
   return (
     <div className={classes.root}>
