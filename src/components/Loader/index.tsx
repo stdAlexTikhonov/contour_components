35839ec84d10a2e48b199709087094dfcc98beb1
stdAlexTikhonov <std1992@mail.ru>
2020-05-12 13:44:00 +0300

    TASK-13 Added svg

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import AutorenewIcon from "@material-ui/icons/Autorenew";
+import { Loader } from "./Loader";
 import { useStyles } from "./styles";
 
 export const LoaderComponent: React.FC = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AutorenewIcon className={classes.loader} />
+      <div className={classes.loader}>
+        <Loader />
+      </div>
     </div>
   );
 };
