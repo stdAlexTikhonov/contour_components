c2a80f6b24cf8a46c422d4124990eaeb9e63eb36
stdAlexTikhonov <std1992@mail.ru>
2020-05-19 18:31:05 +0300

    TASK-19 TreeComponent structure

--- src/components/Tree/index.tsx
+++ src/components/Tree/index.tsx
@@ -1,82 +1,7 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
 import { connect } from "react-redux";
-import { makeStyles } from "@material-ui/core/styles";
-import TreeView from "@material-ui/lab/TreeView";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import TreeItem from "@material-ui/lab/TreeItem";
 import { AppState } from "../../store/config_store";
-import { getData } from "../../utils/api";
-import { DataForQuery } from "../../utils/types";
-import { ITEMS } from "../../utils/constants";
-
-export interface Props {}
-
-export type IProps = Props & LinkStateToProps;
-
-export interface LinkStateToProps {
-  items: any;
-  session: string | undefined;
-  language: string;
-}
-
-const useStyles = makeStyles({
-  root: {
-    height: 240,
-    flexGrow: 1,
-    maxWidth: 400,
-    paddingTop: 65,
-  },
-});
-
-export const FoldersNavigator: React.FC<IProps> = ({
-  items,
-  session,
-  language,
-}) => {
-  const classes = useStyles();
-  const [tree, setTree] = useState(items);
-  const { solution, project } = useParams();
-
-  const addToTree = async (item: any) => {
-    const data = await getData({
-      method: ITEMS,
-      p_folder: item.code,
-      solution,
-      project,
-      session,
-      language,
-    });
-
-    item.items = data.items;
-    const new_tree = tree.slice();
-    setTree(new_tree);
-  };
-
-  const renderTree = (items: any) => {
-    return items.map((item: any) => (
-      <TreeItem
-        nodeId={item.code}
-        label={item.caption}
-        key={item.code}
-        onClick={() => item.type === "folder" && addToTree(item)}
-      >
-        {item.items && renderTree(item.items)}
-      </TreeItem>
-    ));
-  };
-
-  return (
-    <TreeView
-      className={classes.root}
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-    >
-      {renderTree(tree)}
-    </TreeView>
-  );
-};
+import { LinkStateToProps } from "./types";
+import { TreeComponent } from "./TreeComponent";
 
 const mapStateToProps = (state: AppState): LinkStateToProps => ({
   items: state.items,
@@ -84,4 +9,4 @@ const mapStateToProps = (state: AppState): LinkStateToProps => ({
   language: state.languages.current,
 });
 
-export const Tree = connect(mapStateToProps)(FoldersNavigator);
+export const Tree = connect(mapStateToProps)(TreeComponent);
---
+++ src/components/Tree/styles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles({
+  root: {
+    height: 240,
+    flexGrow: 1,
+    maxWidth: 400,
+    paddingTop: 65,
+  },
+});
