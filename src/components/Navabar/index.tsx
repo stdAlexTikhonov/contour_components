cc5f4c6b168bf77d8e3c3aca6a31f6433c983e84
stdAlexTikhonov <std1992@mail.ru>
2020-05-19 14:35:33 +0300

    TASK-19 Removed useless component import

--- src/components/Navabar/index.tsx
+++ src/components/Navabar/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import { Link } from "react-router-dom";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { useStyles } from "./styles";
 import { LanguageSelector } from "../LanguageSelector";
