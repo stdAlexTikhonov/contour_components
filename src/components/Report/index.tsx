79d8d4cd26648e037a7b5f21466bf329da31c1c9
stdAlexTikhonov <std1992@mail.ru>
2020-05-21 14:57:42 +0300

    TASK-25 Report fix

--- src/components/Report/index.tsx
+++ src/components/Report/index.tsx
@@ -26,15 +26,23 @@ const mapDispatchToProps = (dispatch: any): LinkDispatchToProps => ({
   handleDataQuery: async (data_for_query: DataForQuery) => {
     const reportData = await getData(data_for_query);
 
-    if (reportData.success && reportData.type) {
-      dispatch(setReportType(reportData.type));
+    //if success and response have type property then we can save type
+    if (reportData.success) {
+      reportData.type && dispatch(setReportType(reportData.type));
+
+      //if we got dashboard prop then we can save data for dashboard
       reportData.dashboard &&
         dispatch(setDashboard(formatGeometry(reportData.dashboard)));
-      reportData.tab_item && dispatch(setTabItem(reportData.tab_item));
-    }
 
-    reportData.path && dispatch(setBreadcrumbs(reportData.path));
-    data_for_query.type && dispatch(setTabs(reportData.items));
+      //...
+      reportData.tab_item && dispatch(setTabItem(reportData.tab_item));
+
+      //set breadcrumbs
+      reportData.path && dispatch(setBreadcrumbs(reportData.path));
+
+      //if we got type in query then setTabs
+      data_for_query.type && dispatch(setTabs(reportData.items));
+    }
   },
 });
 
