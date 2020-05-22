e68ec434ed9a4315b1fb6e25cae12d5d662e8b8b
stdAlexTikhonov <std1992@mail.ru>
2020-05-22 11:43:02 +0300

    TASK-26 Fixed links in breadcrumbs

--- src/components/Breadcrumbs/BreadcrumbsComponent.tsx
+++ src/components/Breadcrumbs/BreadcrumbsComponent.tsx
@@ -8,13 +8,28 @@ import { Breadcrumb } from "../../types/actions";
 
 export const BreadcrumbsComponent: React.FC<IProps> = ({ breadcrumbs }) => {
   const len = breadcrumbs.length - 1;
+  const bredcrumbs_transformed = breadcrumbs.map((item) => item.code);
   const classes = useStyles();
+
   return (
     <Breadcrumbs aria-label="breadcrumb" maxItems={3} className={classes.root}>
       {breadcrumbs.map((breadcrumb: Breadcrumb, i: number) => {
+        if (["project", "report"].includes(breadcrumb.type))
+          bredcrumbs_transformed[i] =
+            breadcrumb.type + "/" + bredcrumbs_transformed[i];
+
+        const filtered = bredcrumbs_transformed.slice(0, i + 1);
+
+        const link = filtered.join("/");
+
+        console.log(link);
         if (i < len)
           return (
-            <Link color="inherit" href={"/"} key={i}>
+            <Link
+              color="inherit"
+              href={process.env.REACT_APP_DEV_URL + link}
+              key={i}
+            >
               {breadcrumb.caption}
             </Link>
           );
--- src/components/Navabar/styles.ts
+++ src/components/Navabar/styles.ts
@@ -1,5 +1,4 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { items } from "../../reducers";
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
