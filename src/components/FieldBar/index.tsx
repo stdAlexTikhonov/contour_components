f4c7f20caf5a55d7807d2dbddd809f3cd27b3929
stdAlexTikhonov <std1992@mail.ru>
2020-05-28 15:28:02 +0300

    TASK-34 DnD Dims

--- src/components/FieldBar/index.tsx
+++ src/components/FieldBar/index.tsx
@@ -40,7 +40,6 @@ export const FieldBar: React.FC<IProps> = ({ show, position, facts, dims }) => {
                     >
                       {(provided) => (
                         <ListItem
-                          key={item.code}
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
@@ -60,16 +59,37 @@ export const FieldBar: React.FC<IProps> = ({ show, position, facts, dims }) => {
           </Box>
           <Box className={classes.main} style={{ overflow: "scroll" }}>
             <b>Измерения</b>
-            <List dense={false}>
-              {dims.map((item: any) => (
-                <ListItem key={item.code}>
-                  <ListItemIcon>
-                    <DragIndicatorIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={item.Caption} />
-                </ListItem>
-              ))}
-            </List>
+            <Droppable droppableId="droppable2">
+              {(provided) => (
+                <List
+                  dense={false}
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {dims.map((item: any, i: number) => (
+                    <Draggable
+                      key={item.code}
+                      draggableId={item.code}
+                      index={i}
+                    >
+                      {(provided) => (
+                        <ListItem
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <ListItemIcon>
+                            <DragIndicatorIcon />
+                          </ListItemIcon>
+                          <ListItemText primary={item.Caption} />
+                        </ListItem>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </List>
+              )}
+            </Droppable>
           </Box>
         </Box>
         <Box className={classes.main} />
