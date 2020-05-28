ee770bb6f03b3c4615f23da1c9e15910a5faf760
stdAlexTikhonov <std1992@mail.ru>
2020-05-28 15:19:21 +0300

    TASK-34 DnD Facts

--- src/components/FieldBar/index.tsx
+++ src/components/FieldBar/index.tsx
@@ -7,48 +7,73 @@ import ListItemText from "@material-ui/core/ListItemText";
 import DragIndicatorIcon from "@material-ui/icons/DragIndicator";
 import { IProps, POSITIONS_TYPE } from "./types";
 import { useStyles } from "./styles";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 export const FieldBar: React.FC<IProps> = ({ show, position, facts, dims }) => {
   const classes = useStyles();
   let pos = position.split("-")[0] as POSITIONS_TYPE;
   pos = pos === "row" ? "column" : "row";
   return (
-    <Box className={classes.root} style={{ flexDirection: position }}>
-      <Box
-        className={classes.aside}
-        style={{ display: show ? "flex" : "none", flexDirection: pos }}
-      >
+    <DragDropContext onDragEnd={() => alert("end")}>
+      <Box className={classes.root} style={{ flexDirection: position }}>
         <Box
           className={classes.aside}
-          style={{ display: "block", overflow: "scroll" }}
+          style={{ display: show ? "flex" : "none", flexDirection: pos }}
         >
-          <b>Факты</b>
-          <List dense={false}>
-            {facts.map((item: any) => (
-              <ListItem key={item.code}>
-                <ListItemIcon>
-                  <DragIndicatorIcon />
-                </ListItemIcon>
-                <ListItemText primary={item.Caption} />
-              </ListItem>
-            ))}
-          </List>
+          <Box
+            className={classes.aside}
+            style={{ display: "block", overflow: "scroll" }}
+          >
+            <b>Факты</b>
+            <Droppable droppableId="droppable">
+              {(provided) => (
+                <List
+                  dense={false}
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {facts.map((item: any, i: number) => (
+                    <Draggable
+                      key={item.code}
+                      draggableId={item.code}
+                      index={i}
+                    >
+                      {(provided) => (
+                        <ListItem
+                          key={item.code}
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <ListItemIcon>
+                            <DragIndicatorIcon />
+                          </ListItemIcon>
+                          <ListItemText primary={item.Caption} />
+                        </ListItem>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </List>
+              )}
+            </Droppable>
+          </Box>
+          <Box className={classes.main} style={{ overflow: "scroll" }}>
+            <b>Измерения</b>
+            <List dense={false}>
+              {dims.map((item: any) => (
+                <ListItem key={item.code}>
+                  <ListItemIcon>
+                    <DragIndicatorIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={item.Caption} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
         </Box>
-        <Box className={classes.main} style={{ overflow: "scroll" }}>
-          <b>Измерения</b>
-          <List dense={false}>
-            {dims.map((item: any) => (
-              <ListItem key={item.code}>
-                <ListItemIcon>
-                  <DragIndicatorIcon />
-                </ListItemIcon>
-                <ListItemText primary={item.Caption} />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
+        <Box className={classes.main} />
       </Box>
-      <Box className={classes.main} />
-    </Box>
+    </DragDropContext>
   );
 };
