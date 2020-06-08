4434ab4061b21e2f58cd5641546616a3a7cffc71
stdAlexTikhonov <std1992@mail.ru>
2020-06-08 17:57:02 +0300

    TASK-39 Setting Fact by clicking OK

--- src/components/Fact/FactComponent.tsx
+++ src/components/Fact/FactComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { useParams } from "react-router-dom";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -23,6 +23,7 @@ export const FactComponent: React.FC<IProps> = ({
   language,
   visibleFacts,
 }) => {
+  const my_ref = useRef<any>();
   const { solution, project, report } = useParams();
   const [selectAll, setSelectAll] = useState(
     visibleFacts.length === items.length
@@ -36,40 +37,46 @@ export const FactComponent: React.FC<IProps> = ({
     ...items.map((item: any) => item.Caption),
   ]);
   const [val, setVal] = useState(selected.map((item: any) => item.Caption));
+  const [factsForQuery, setFactsForQuery] = useState<string[]>([]);
+
+  const handleClick = () => my_ref.current.click();
 
   const FilterControlPanel = () => (
     <div style={{ display: "flex", justifyContent: "flex-end" }}>
-      <Button>Ok</Button>
+      <Button onClick={handleClick}>Ok</Button>
       <Button>Cancel</Button>
     </div>
   );
 
   const handleChange = (event: object, value: any, reason: string) => {
     //Проверка - есть ли в списке Select All
-    const check_select_all = value.includes("Select All");
+    // const check_select_all = value.includes("Select All");
 
     //Проверка - соответствует ли чекбокс select All предыдущему состоянию
-    const flag = check_select_all !== selectAll;
-    if (flag) setSelectAll(check_select_all);
-    else {
-      setVal(value);
-      const facts_filtered = items.filter((item: any) => value.includes(item));
+    // const flag = check_select_all !== selectAll;
+    // if (flag) setSelectAll(check_select_all);
+    // else {
 
-      const facts_for_server = facts_filtered.map((item: any) => item.code);
+    setVal(value);
+    const facts_filtered = items.filter((item: any) =>
+      value.includes(item.Caption)
+    );
 
-      // // Установка фильтра на сервере
-      handleDataQuery({
-        method: SET_FACTS,
-        session,
-        language,
-        solution,
-        project,
-        report,
-        slice,
-        view,
-        visibleFacts: facts_for_server,
-      });
-    }
+    const facts_for_server = facts_filtered.map((item: any) => item.code);
+
+    // // Установка фильтра на сервере
+    // handleDataQuery({
+    //   method: SET_FACTS,
+    //   session,
+    //   language,
+    //   solution,
+    //   project,
+    //   report,
+    //   slice,
+    //   view,
+    //   visibleFacts: facts_for_server,
+    // });
+    setFactsForQuery(facts_for_server);
   };
 
   useEffect(() => {
@@ -79,76 +86,85 @@ export const FactComponent: React.FC<IProps> = ({
       facts_for_server = items.map((item: any) => item.code);
     } else setVal([]);
 
-    handleDataQuery({
-      method: SET_FACTS,
-      session,
-      language,
-      solution,
-      project,
-      report,
-      slice,
-      view,
-      visibleFacts: facts_for_server,
-    });
+    setFactsForQuery(facts_for_server);
   }, [selectAll]);
 
   return (
-    <Autocomplete
-      multiple
-      id="size-small-outlined"
-      size="small"
-      open={open}
-      renderTags={() => false}
-      onChange={handleChange}
-      value={val}
-      options={options as string[]}
-      onOpen={async () => {
-        setOpen(true);
-        await sleep(500);
-        const popper = document.getElementsByClassName(
-          "MuiAutocomplete-popper"
-        )[0];
-        // popper.appendChild(controlPanel);
-        const controlPanel = document.createElement("div");
-        controlPanel.style.width = "100%";
-        ReactDOM.render(FilterControlPanel(), controlPanel);
-        popper.appendChild(controlPanel);
-      }}
-      onClose={(event: object, reason: string) => {
-        if (reason === "blur" || reason === "toggleInput") setOpen(false);
-      }}
-      renderOption={(option, { selected }) => (
-        <React.Fragment>
-          <Checkbox
-            icon={icon}
-            checkedIcon={checkedIcon}
-            style={{ marginRight: 8 }}
-            checked={selected}
-            color="primary"
-            inputProps={{ "aria-label": "secondary checkbox" }}
+    <>
+      <Autocomplete
+        multiple
+        size="small"
+        open={open}
+        renderTags={() => false}
+        onChange={handleChange}
+        value={val}
+        options={options as string[]}
+        onOpen={async () => {
+          setOpen(true);
+          await sleep(500);
+          const popper = document.getElementsByClassName(
+            "MuiAutocomplete-popper"
+          )[0];
+          // popper.appendChild(controlPanel);
+          const controlPanel = document.createElement("div");
+          controlPanel.style.width = "100%";
+          ReactDOM.render(FilterControlPanel(), controlPanel);
+          popper.appendChild(controlPanel);
+        }}
+        onClose={(event: object, reason: string) => {
+          if (reason === "toggleInput") setOpen(false);
+        }}
+        renderOption={(option, { selected }) => (
+          <React.Fragment>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
+              color="primary"
+              inputProps={{ "aria-label": "secondary checkbox" }}
+            />
+            {option}
+          </React.Fragment>
+        )}
+        style={{
+          minWidth: 275,
+          maxWidth: 275,
+          minHeight: 50,
+          overflow: "hidden",
+          padding: 5,
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            label="Факты"
+            placeholder="Type here"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: params.InputProps.endAdornment,
+            }}
           />
-          {option}
-        </React.Fragment>
-      )}
-      style={{
-        minWidth: 275,
-        maxWidth: 275,
-        minHeight: 50,
-        overflow: "hidden",
-        padding: 5,
-      }}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          variant="outlined"
-          label="Факты"
-          placeholder="Type here"
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: params.InputProps.endAdornment,
-          }}
-        />
-      )}
-    />
+        )}
+      />
+
+      <Button
+        style={{ position: "absolute", top: 0, left: 0 }}
+        ref={my_ref}
+        onClick={() => {
+          handleDataQuery({
+            method: SET_FACTS,
+            session,
+            language,
+            solution,
+            project,
+            report,
+            slice,
+            view,
+            visibleFacts: factsForQuery,
+          });
+        }}
+      ></Button>
+    </>
   );
 };
