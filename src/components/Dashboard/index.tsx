e339d4d3aed98b554daa0949b78c275a4382535e
stdAlexTikhonov <std1992@mail.ru>
2020-06-16 12:41:48 +0300

    TASK-47 Metadata for right views

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@material-ui/core/Box";
 import { IProps } from "./types";
 import { useStyles } from "./styles";
@@ -7,34 +7,49 @@ import { View } from "../View";
 
 export const Dashboard: React.FC<IProps> = ({ dashboard, metadata }) => {
   const classes = useStyles();
+  const [dataForView, setDataForView] = useState<any[] | null>(null);
 
   useEffect(() => {
-    console.log(metadata);
-    //   cells.sort((a: any, b: any) =>
-    //   a.row === b.row ? a.col - b.col : a.row - b.row
-    // );
+    if (metadata && dashboard) {
+      let data = [];
+      for (let i = 0; i < metadata.length; i++)
+        data.push({ ...dashboard!.cells[i], ...metadata[i] });
+
+      data.sort((a: any, b: any) =>
+        a.row === b.row ? a.col - b.col : a.row - b.row
+      );
+      setDataForView(data);
+    }
   }, [metadata]);
 
-  return (
-    dashboard &&
-    dashboard!.cells.map((item: any, i: number) => (
-      <Box
-        key={i}
-        className={classes.block}
-        width={item.w}
-        height={item.h + item.hcu}
-        style={{ float: item.float }}
-      >
-        {metadata ? (
-          <View metadata={{ ...metadata[i], ...dashboard.cells[i] }} />
-        ) : (
-          <Box display="flex" height="100%">
-            <Box margin="auto">
-              <Loader />
+  return dataForView
+    ? dataForView.map((item: any, i: number) => (
+        <Box
+          key={i}
+          className={classes.block}
+          width={item.w}
+          height={item.h + item.hcu}
+          style={{ float: item.float }}
+        >
+          <View metadata={item} />
+        </Box>
+      ))
+    : dashboard &&
+        dashboard!.cells.map((item: any, i: number) => (
+          <Box
+            key={i}
+            className={classes.block}
+            width={item.w}
+            height={item.h + item.hcu}
+            style={{ float: item.float }}
+          >
+            {/* <View metadata={{ ...metadata[i], ...dashboard.cells[i] }} /> */}
+
+            <Box display="flex" height="100%">
+              <Box margin="auto">
+                <Loader />
+              </Box>
             </Box>
           </Box>
-        )}
-      </Box>
-    ))
-  );
+        ));
 };
