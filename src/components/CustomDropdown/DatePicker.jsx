58f56bc8d8460fbfa38377bd5aff6d54545ebf76
stdAlexTikhonov <std1992@mail.ru>
2020-06-19 14:09:15 +0300

    TASK-53 refactor datepicker

--- src/components/CustomDropdown/DatePicker.jsx
+++ src/components/CustomDropdown/DatePicker.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useCallback } from "react";
-import TextField from "@material-ui/core/TextField";
 import MultipleDatePicker from "../MultipleDatePicker";
-import IconButton from "@material-ui/core/IconButton";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
 import { useStyles } from "./styles";
 import ThemeProvider from "./ThemeProvider";
 import moment from "moment";
