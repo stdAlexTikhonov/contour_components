e97a12442851611e8ec56520130a7af4f85aca52
stdAlexTikhonov <std1992@mail.ru>
2020-07-19 22:06:21 +0300

    TASK-67 Showed first column

--- src/components/DemoTable/DemoTableComponent.jsx
+++ src/components/DemoTable/DemoTableComponent.jsx
@@ -3,37 +3,39 @@ import { Resizable } from "react-resizable";
 import "./index.scss";
 import ReactVirtualSizeTable from "../VirtualTable";
 
-const columnNames = [];
-const tableData = [];
-for (let i = 0; i < 3; i++) {
-  columnNames.push(`column_${i}`);
-  const row = {};
-  for (let j = 0; j < 100; j++) {
-    row[`column_${j}`] = i + j;
-  }
-  tableData.push(row);
-}
-
 export class DemoComponent extends Component {
   state = {
     widths: [],
+    columnNames: [],
+    tableData: [],
   };
+
+  componentDidMount() {
+    const { hierarchy } = this.props;
+    const root = hierarchy[hierarchy.root];
+    this.setState({
+      columnNames: [{ label: root.label, dataKey: hierarchy.root }],
+      tableData: root.Captions.map((item) => ({
+        [`${hierarchy.root}`]: item,
+      })),
+    });
+  }
   onResize = (index, value) => {
-    const { widths } = this.state;
+    const { widths, columnNames } = this.state;
     widths[index] = value.size.width;
     this.setState({
       widths: { ...widths },
     });
   };
   renderCell = ({ columnIndex, key, rowIndex, style }) => {
-    const { widths } = this.state;
-    const field = columnNames[columnIndex];
+    const { widths, columnNames, tableData } = this.state;
+    const field = columnNames[columnIndex].label;
+    const dataKey = columnNames[columnIndex].dataKey;
     const text =
       rowIndex === 0
         ? field
-        : tableData[rowIndex] && tableData[rowIndex][field];
+        : tableData[rowIndex] && tableData[rowIndex][dataKey];
 
-    console.log("Hello");
     return (
       <Fragment key={key}>
         {rowIndex === 0 ? (
@@ -61,31 +63,34 @@ export class DemoComponent extends Component {
     );
   };
   render() {
-    const { widths } = this.state;
-    // 同步
-    const widthArray = columnNames.map(() => 140);
-    for (let key in widths) {
-      widthArray[key] = widths[key];
-    }
-    let width = window.innerWidth - 50;
+    const { widths, columnNames, tableData } = this.state;
 
-    const totalWidths = widthArray.reduce((a, b) => a + b);
-    if (totalWidths < width) {
-      width = totalWidths - 1;
+    // 同步
+    if (columnNames.length > 0) {
+      const widthArray = columnNames.map(() => 140);
+      for (let key in widths) {
+        widthArray[key] = widths[key];
+      }
+      let width = window.innerWidth - 50;
+      const totalWidths = widthArray.reduce((a, b) => a + b);
+      if (totalWidths < width) {
+        width = totalWidths - 1;
+      }
+      return (
+        <div className={"res-table"}>
+          <ReactVirtualSizeTable
+            onCell={(value) => this.renderCell(value, columnNames, tableData)}
+            columnCount={columnNames.length}
+            widths={widthArray}
+            height={250}
+            rowCount={tableData.length}
+            rowHeight={36}
+            width={width}
+            style={{ width: "100%", padding: "0 20px" }}
+          />
+        </div>
+      );
     }
-    return (
-      <div className={"res-table"}>
-        <ReactVirtualSizeTable
-          onCell={(value) => this.renderCell(value, columnNames, tableData)}
-          columnCount={columnNames.length}
-          widths={widthArray}
-          height={250}
-          rowCount={tableData.length}
-          rowHeight={36}
-          width={width}
-          style={{ width: "100%", padding: "0 20px" }}
-        />
-      </div>
-    );
+    return null;
   }
 }
