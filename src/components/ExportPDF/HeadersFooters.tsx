eec9dd79d7857176e1549383f8030bc8fe69ccdc
stdAlexTikhonov <std1992@mail.ru>
2020-08-05 17:25:00 +0300

    TASK-69 Headers/Footers

--- src/components/ExportPDF/HeadersFooters.tsx
+++ src/components/ExportPDF/HeadersFooters.tsx
@@ -1,3 +1,182 @@
 import React from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Radio from "@material-ui/core/Radio";
+import Checkbox from "@material-ui/core/Checkbox";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 
-export const HeadersFooters = () => <div>Headers/Foooters</div>;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    formControl: {
+      margin: theme.spacing(3),
+    },
+  })
+);
+
+export const HeadersFooters = () => {
+  const classes = useStyles();
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setAge(event.target.value as string);
+  };
+
+  const [state, setState] = React.useState({
+    columns: true,
+    rows: false,
+    title: false,
+    header: false,
+    footer: false,
+  });
+
+  const [group, setGroup] = React.useState({
+    colontitules: false,
+    changeScale: false,
+  });
+
+  const handleChangeCheckboxes = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const handleChangeCheckboxes2 = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setGroup({ ...group, [event.target.name]: event.target.checked });
+  };
+
+  const { columns, rows, title, header, footer } = state;
+  const { colontitules, changeScale } = group;
+
+  return (
+    <>
+      <FormControl className={classes.formControl}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={colontitules}
+              onChange={handleChangeCheckboxes2}
+              name="colontitules"
+            />
+          }
+          label="Headers/Footers"
+        />
+        <FormGroup
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Left</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Left</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Left</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </FormGroup>
+        <FormGroup
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Left</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Left</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Left</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </FormGroup>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={changeScale}
+              onChange={handleChangeCheckboxes2}
+              name="changeScale"
+            />
+          }
+          label="Change scale along with the document"
+        />
+      </FormControl>
+    </>
+  );
+};
