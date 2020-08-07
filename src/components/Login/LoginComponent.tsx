882a33c6df09c0d700d369d7496cc2f32195bd72
stdAlexTikhonov <std1992@mail.ru>
2020-08-07 11:55:55 +0300

    TASK-69 Added themeProvider in Login and RegistrationForm

--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -3,6 +3,7 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { useStyles } from "./styles";
 import { IProps } from "./types";
+import ThemeProvider from "../CustomDropdown/ThemeProvider";
 
 export const LoginComponent: React.FC<IProps> = ({
   handleLogin,
@@ -13,33 +14,34 @@ export const LoginComponent: React.FC<IProps> = ({
   const refPassword = useRef<HTMLInputElement | undefined>();
 
   return (
-    <div className={classes.container}>
-      <form className={classes.root} noValidate={true} autoComplete="off">
-        <TextField
-          id="filled-basic"
-          label="Login"
-          variant="outlined"
-          inputRef={refLogin}
-        />
+    <ThemeProvider>
+      <div className={classes.container}>
+        <form className={classes.root} noValidate={true} autoComplete="off">
+          <TextField
+            id="filled-basic"
+            label="Login"
+            variant="outlined"
+            inputRef={refLogin}
+          />
 
-        <TextField
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          type="password"
-          inputRef={refPassword}
-        />
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            type="password"
+            inputRef={refPassword}
+          />
 
-        <Button
-          variant="contained"
-          style={{ backgroundColor: "#003366" }}
-          color="primary"
-          disabled={logged_in}
-          onClick={() => handleLogin(refLogin.current, refPassword.current)}
-        >
-          Login
-        </Button>
-      </form>
-    </div>
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={logged_in}
+            onClick={() => handleLogin(refLogin.current, refPassword.current)}
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    </ThemeProvider>
   );
 };
