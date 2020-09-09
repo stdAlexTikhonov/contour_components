eea8d8221de6c4ae3693fd5a9b8849fe2bf33235
stdAlexTikhonov <std1992@mail.ru>
2020-09-09 17:48:33 +0300

    TASK-80 FiX

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -192,7 +192,7 @@ export const formatGeometry = (dashboard: any) => {
     for (let i = 0; i < h_.length; i++) h += h_[i];
 
     elem.h = h;
-    elem.h_px = window.innerHeight * (h / 100);
+    elem.h_px = rows.cu === "%" ? window.innerHeight * (h / 100) : h;
 
     return {
       ...elem,
