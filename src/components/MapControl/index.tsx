56bf4e82ee340212a7478becd43b801dc22f6b6b
stdAlexTikhonov <std1992@mail.ru>
2020-09-15 17:59:50 +0300

    TASK-84 Scaling

--- src/components/MapControl/index.tsx
+++ src/components/MapControl/index.tsx
@@ -11,7 +11,7 @@ export const MapControl: React.FC<Props> = ({
   coords,
   setCoords,
 }) => {
-  const [cursor, setCursor] = useState(false);
+  const [area, setArea] = useState(false);
   const [areaWidth, setAreaWidth] = useState(0);
   const [areaHeight, setAreaHeight] = useState(0);
   const [mousePressed, setMousePressed] = useState(false);
@@ -21,7 +21,7 @@ export const MapControl: React.FC<Props> = ({
   const [startPositionY, setStartPositionY] = useState(0);
 
   const mouseDownHandler = (e: any) => {
-    if (cursor) {
+    if (area) {
       let rect = e.target.getBoundingClientRect();
       setMousePressed(true);
       setStartPositionX(e.clientX - rect.left);
@@ -35,16 +35,32 @@ export const MapControl: React.FC<Props> = ({
   };
 
   const mouseUpHandler = (e: any) => {
-    if (cursor) {
+    const coeffX = (coords[2] - coords[0]) / width;
+    const coeffY = (coords[3] - coords[1]) / height;
+
+    if (area) {
       setMousePressed(false);
+
+      //Расчёты
+      const diffX = width - (startPositionX + areaWidth);
+      const diffY = height - (startPositionY + areaHeight);
+      const new_left_position = coords[0] + startPositionX * coeffX;
+      const new_top_position = coords[1] - startPositionY * coeffY;
+      const new_right_position = coords[2] + startPositionX * coeffX; //+ diffX * coeffX;
+      const new_bottom_position = coords[3] - startPositionY * coeffY; //+ diffY * coeffY;
+
+      setCoords([
+        new_left_position,
+        new_top_position,
+        new_right_position,
+        new_bottom_position,
+      ]);
+
       setAreaWidth(0);
       setAreaHeight(0);
     } else {
       const diffX = e.clientX - startPositionX;
       const diffY = e.clientY - startPositionY;
-
-      const coeffX = (coords[2] - coords[0]) / width;
-      const coeffY = (coords[3] - coords[1]) / height;
 
       const new_left_position = coords[0] - diffX * coeffX;
       const new_top_position = coords[1] + diffY * coeffY;
@@ -61,7 +77,7 @@ export const MapControl: React.FC<Props> = ({
   };
 
   const mouseMoveHandler = (e: any) => {
-    if (cursor && mousePressed) {
+    if (area && mousePressed) {
       let rect = e.target.getBoundingClientRect();
       setAreaWidth(e.clientX - rect.left - startPositionX);
       setAreaHeight(e.clientY - rect.top - startPositionY);
@@ -73,7 +89,7 @@ export const MapControl: React.FC<Props> = ({
         style={{
           width,
           height,
-          cursor: cursor ? "crosshair" : "move",
+          cursor: area ? "crosshair" : "move",
         }}
         onMouseDown={mouseDownHandler}
         onMouseUp={mouseUpHandler}
@@ -84,7 +100,7 @@ export const MapControl: React.FC<Props> = ({
         className={classes.btn}
         style={{ left: 0, top: 0 }}
         onClick={(e) => {
-          setCursor(false);
+          setArea(false);
         }}
       >
         <OpenWithIcon fontSize="small" />
@@ -94,12 +110,12 @@ export const MapControl: React.FC<Props> = ({
         className={classes.btn}
         style={{ left: 0, top: 35 }}
         onClick={(e) => {
-          setCursor(true);
+          setArea(true);
         }}
       >
         <ZoomInIcon fontSize="small" />
       </IconButton>
-      {cursor && (
+      {area && (
         <div
           style={{
             position: "absolute",
